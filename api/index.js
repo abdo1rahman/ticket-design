fcd9c90b828ce28f13f3d9b1394ba679980ce1eb
abdo1rahman <abdoasem2055@gmail.com>
2025-04-18 09:44:36 +0200

    vercel

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
-// const port = 3000;
+const port = 3000;
 
 const app = express();
 const storage = multer.memoryStorage();
